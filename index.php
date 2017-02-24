27a0f99d2fc8cf8b9620daf47b3c720f4c79726b
nFnK <wpekle@gmail.com>
2017-02-24 17:10:34 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+include ("pdodb.php");
+$crud = new CRUD();
+$denemetb = $crud->select("deneme","*",null,null,null,"0,5");
+//($table, $rows = '*', $join = null, $where = null, $order = null, $limit = null)
+if (count($denemetb) > 0)
+{
+echo '<table class="table table-bordered table-striped">
+                        <tr>
+                            <th>No.</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Email Address</th>
+                        </tr>';
+
+    $number = 1;
+    foreach ($denemetb as $ic)
+    {
+       echo '<tr>
+                <td>' . $number . '</td>
+                <td>' . $ic['ekleyen'] . '</td>
+                <td>' . $ic['baslik'] . '</td>
+                <td>' . $ic['eklenme'] . '</td>
+         
+            </tr>';
+        $number++;
+    }
+echo '</table>';
+} else
+{
+echo '<tr><td colspan="6">Hata select</td></tr>';
+}
+
+
+// select end
+
+echo "<hr />";
+
+
+
+
+// detay
+echo "<hr />";
+$detay = json_decode($crud->details(39));
+echo $detay->baslik;
+
+/* // insert
+$crud->insert('deneme',array('ekleyen'=>'insertx','baslik'=>''.rand().'','onay'=>'2','ip'=>'212.22.22.2'));  
+echo "<br />".  $crud->insertid();*/
+$crud->update('deneme','id=42',array('ekleyen'=>'upladık','eklenme'=>'2015-01-01 14:06:41','baslik'=>'up '.rand().'','onay'=>'3','ip'=>'111.22.22.2'));  
+
+?>
---
+++ pdodb.php
@@ -0,0 +1,115 @@
+<?php
+class Database
+{
+    private static $dbName = '_pdo';
+    private static $dbHost = 'localhost';
+    private static $dbUsername = 'root';
+    private static $dbUserPassword = 'root';
+    private static $dbset = 'utf8';
+
+    private static $cont = null;
+
+    public function __construct()
+    {
+        die('Init function is not allowed');
+    }
+
+    public static function connect()
+    {
+        // One connection through whole application
+        if (null == self::$cont)
+        {
+            try
+            {
+                self::$cont = new PDO("mysql:host=" . self::$dbHost . ";" . "dbname=" . self::$dbName,
+                self::$dbUsername, self::$dbUserPassword);
+                self::$cont->exec("SET NAMES " . self::$dbset . "");
+            }
+            catch (PDOException $e)
+            {
+                die("Hata bağlantı yok " /*.$e->getMessage()*/);
+            }
+        }
+        return self::$cont;
+    }
+
+    public static function disconnect()
+    {
+        self::$cont = null;
+    }
+}
+
+
+class CRUD extends Database
+{
+
+    protected $db;
+
+    function __construct()
+    {
+        $this->db = parent::connect();
+    }
+
+    function __destruct()
+    {
+        $this->db = parent::disconnect();
+    }
+
+
+   public function insert($table,$params=array()){
+     $query = $this->db->prepare('INSERT INTO `'.$table.'` ('.implode(',',array_keys($params)).') VALUES (:'.implode(',:',array_keys($params)).')');
+    $query->execute($params);
+    return  $this->db->lastInsertId(); 
+    }
+
+    public function insertid() {
+    return $this->db->lastInsertId();
+    }  
+    
+    public function update($table,$id,$params=array()){
+     $ikey = ''; foreach($params as $zzm => $valz) {$ikey .= $zzm." = :".$zzm.", ";} $ikey = rtrim($ikey,", ");   
+     $query = $this->db->prepare('UPDATE `'.$table.'` set '.$ikey.' where  '.$id.'');
+   $query->execute($params);
+   return $query->rowCount();
+      }
+
+
+   
+    	public function select($table, $rows = '*', $join = null, $where = null, $order = null, $limit = null){
+		$q = 'SELECT '.$rows.' FROM '.$table;
+		if($join != null){	$q .= ' JOIN '.$join;}
+        if($where != null){	$q .= ' WHERE '.$where;	}
+        if($order != null){  $q .= ' ORDER BY '.$order;	}
+        if($limit != null){ $q .= ' LIMIT '.$limit; }
+          $query = $this->db->prepare($q);
+        $query->execute();
+        $data = array();
+        while ($row = $query->fetch(PDO::FETCH_ASSOC))
+        {
+            $data[] = $row;
+        }
+        return $data;
+        }
+
+
+    public function delete($user_id)
+    {
+        $query = $this->db->prepare("DELETE FROM deneme WHERE id = :id");
+        $query->bindParam("id", $user_id, PDO::PARAM_STR);
+        $query->execute();
+    }
+
+
+
+
+    public function details($user_id)
+    {
+        $query = $this->db->prepare("SELECT * FROM deneme WHERE id = :id");
+        $query->bindParam("id", $user_id, PDO::PARAM_STR);
+        $query->execute();
+        return json_encode($query->fetch(PDO::FETCH_ASSOC));
+    }
+}
+
+
+?>
